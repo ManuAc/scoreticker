d36a66a0975e7b107e35e1848bd027f8c70ea7c5
ManuAc <manuac93@gmail.com>
2023-07-29 00:30:33 +0100

    Updated public assets and functions

---
+++ functions/main.py
@@ -0,0 +1,64 @@
+# Welcome to Cloud Functions for Firebase for Python!
+# To get started, simply uncomment the below code or create your own.
+# Deploy with `firebase deploy`
+
+from firebase_functions import https_fn, options
+from firebase_admin import firestore
+from firebase_admin import initialize_app
+from flask import request, jsonify
+
+
+# Initialize Firebase Admin SDK
+initialize_app()
+
+
+@https_fn.on_request(
+	cors=options.CorsOptions(cors_origins="*", cors_methods=["get", "post"])
+)
+def add_game_record(request):
+    try:
+        data = request.get_json()
+        date = data.get('date')
+        player1 = data.get('player1')
+        player2 = data.get('player2')
+        score_player1 = data.get('scorePlayer1')
+        score_player2 = data.get('scorePlayer2')
+        winner = data.get('winner')
+
+        # Validate the request data
+        if not all([date, player1, player2, score_player1, score_player2, winner]):
+            return jsonify({'error': 'Invalid request data.'}), 400
+
+        # Add the game record to Firestore
+        game_record_ref = firestore.client().collection('gameRecords').add({
+            'date': date,
+            'player1': player1,
+            'player2': player2,
+            'scorePlayer1': score_player1,
+            'scorePlayer2': score_player2,
+            'winner': winner
+        })
+
+        return jsonify({'status': 'ok'}), 200
+
+    except Exception as e:
+        print('Error adding game record:', e)
+        return jsonify({'error': 'Failed to add game record.'}), 500
+
+
+@https_fn.on_request(
+	cors=options.CorsOptions(cors_origins="*", cors_methods=["get", "post"])
+)
+def get_game_records(request):
+    try:
+        # Retrieve game records from Firestore
+        game_records = []
+        docs = firestore.client().collection('gameRecords').get()
+        for doc in docs:
+            game_records.append(doc.to_dict())
+
+        return jsonify(game_records), 200
+
+    except Exception as e:
+        print('Error retrieving game records:', e)
+        return jsonify({'error': 'Failed to retrieve game records.'}), 500
---
+++ public/ticker.js
@@ -0,0 +1,112 @@
+
+// Function to handle form submission and add game record
+async function handleSubmit(event) {
+  event.preventDefault();
+
+  const form = event.target;
+  const date = form.date.value;
+  const player1 = form.player1.value;
+  const player2 = form.player2.value;
+  const scorePlayer1 = parseInt(form.scorePlayer1.value);
+  const scorePlayer2 = parseInt(form.scorePlayer2.value);
+
+  if (isNaN(scorePlayer1) || isNaN(scorePlayer2)) {
+    alert('Please enter valid scores.');
+    return;
+  }
+
+  const winner = (scorePlayer1 > scorePlayer2) ? player1 : player2;
+
+  try {
+    // Call the Firebase Function to add the game record
+    await addGameRecord(date, player1, player2, scorePlayer1, scorePlayer2, winner);
+
+    // Refresh the scoreboard after adding the game record
+    await loadScoreBoard();
+  } catch (error) {
+    console.error('Failed to add game record:', error.message);
+  }
+}
+
+// Function to call the Firebase Function and add a new game record
+async function addGameRecord(date, player1, player2, scorePlayer1, scorePlayer2, winner) {
+  try {
+    const response = await fetch('/addGameRecord', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        date: date,
+        player1: player1,
+        player2: player2,
+        scorePlayer1: scorePlayer1,
+        scorePlayer2: scorePlayer2,
+        winner: winner
+      })
+    });
+
+    if (!response.ok) {
+      throw new Error('Failed to add game record.');
+    }
+
+    const data = await response.json();
+    console.log('Game record added successfully:', data);
+  } catch (error) {
+    console.error('Error adding game record:', error.message);
+    throw error;
+  }
+}
+
+
+// Function to retrieve game records from Firebase Function
+async function loadScoreBoard() {
+  try {
+    const response = await fetch('/getGameRecords', {
+      // Optional: If you have added a security check for the API key
+      // headers: {
+      //   'Authorization': 'YOUR_API_KEY'
+      // }
+    });
+
+    if (!response.ok) {
+      throw new Error('Failed to fetch game records.');
+    }
+
+    const data = await response.json();
+    const scoreBoard = document.getElementById('score-board');
+    scoreBoard.innerHTML = '';
+
+    data.forEach((game) => {
+      const row = document.createElement('tr');
+      row.innerHTML = `
+        <td>${game.date}</td>
+        <td>${game.player1}</td>
+        <td>${game.player2}</td>
+        <td>${game.scorePlayer1}</td>
+        <td>${game.scorePlayer2}</td>
+        <td class="winner">${game.winner}</td>
+      `;
+
+      if (game.winner === game.player1) {
+        row.querySelector(':nth-child(2)').style.color = 'green'; // Player 1 is the winner
+        row.querySelector(':nth-child(3)').style.color = 'red'; // Player 2 is the loser
+      } else if (game.winner === game.player2) {
+        row.querySelector(':nth-child(2)').style.color = 'red'; // Player 1 is the loser
+        row.querySelector(':nth-child(3)').style.color = 'green'; // Player 2 is the winner
+      }
+
+      scoreBoard.appendChild(row);
+    });
+
+    console.log('Game records loaded successfully:', data);
+  } catch (error) {
+    console.error('Error fetching game records:', error.message);
+  }
+}
+
+// Attach the handleSubmit function to the form submit event
+document.getElementById('game-form').addEventListener('submit', handleSubmit);
+
+// Load the scoreboard on page load
+loadScoreBoard();
